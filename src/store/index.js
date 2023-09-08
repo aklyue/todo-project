8197efd8e90170ca6eb93aea7b595ad85cc3c609
Oleg Lapshin <sryitscool59@gmail.com>
2023-09-08 20:44:11 +0600

    добавлен новый slice в хранилище tasksSlice и подключен к проекту

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,12 @@ import {configureStore} from "@reduxjs/toolkit"
 
 import { counterReducer } from "./counterSlice";
 
+import { tasksReducer } from "./tasksSlice";
+
 const store = configureStore({
     reducer: {
-        counter: counterReducer
+        counter: counterReducer,
+        tasks: tasksReducer
     }
 });
 
